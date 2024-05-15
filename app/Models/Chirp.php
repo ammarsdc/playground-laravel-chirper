e4cb778be3ad582ef77cc42dd6cf2f7ce8c9c17e
Ammar Sani <ammarsdc@gmail.com>
2024-05-15 16:17:36 +0800

    Send notification on new chirp to other users

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
     protected $fillable = ['message'];
+
+    protected $dispatchesEvents = ['created' => ChirpCreated::class];
 
     public function user(): BelongsTo
     {
